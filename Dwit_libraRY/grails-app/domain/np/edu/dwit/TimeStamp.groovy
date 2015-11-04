22bad091371fdf02a5ab0b886b75fb2f333bbbbb
asimregmi <ashim.regmi@deerwalk.edu.np>
2015-11-04 10:21:37 +0545

    Domain classes made

---
+++ Dwit_libraRY/grails-app/domain/np/edu/dwit/TimeStamp.groovy
@@ -0,0 +1,13 @@
+package np.edu.dwit
+
+class TimeStamp {
+
+    Date deadline
+    Role role
+
+    static constraints = {
+
+        deadline min: new Date(), nullable: false
+        role nullable: false
+    }
+}
