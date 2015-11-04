1360e7516a2b1062fbb1c1ec61f3d040be7fc60f
Sushant Gurung <tag35.sgurung@gmail.com>
2015-11-04 10:03:19 +0545

    Books Domain Class updated.

--- Dwit_libraRY/grails-app/domain/np/edu/dwit/Book.groovy
+++ Dwit_libraRY/grails-app/domain/np/edu/dwit/Book.groovy
@@ -4,11 +4,15 @@ class Book {
     String name
     String author
     Integer bookNumber
+    Integer availableQuantity
+    Integer totalQuantity
 
     static constraints = {
         name blank: false
         author blank: false
         bookNumber blank: false
+        availableQuantity blank: false
+        totalQuantity blank: false
     }
     static mapping = {
         version false
