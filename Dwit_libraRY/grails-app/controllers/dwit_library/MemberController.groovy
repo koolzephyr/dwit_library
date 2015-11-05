7a8b3dcbab8811a6b5857c205beffb8303ba8892
koolzephyr <sameer.koirala@deerwalk.edu.np>
2015-11-05 09:43:38 +0545

    Changes in redirect and added user id parameter.

--- Dwit_libraRY/grails-app/controllers/dwit_library/MemberController.groovy
+++ Dwit_libraRY/grails-app/controllers/dwit_library/MemberController.groovy
@@ -16,43 +16,20 @@ class MemberController {
             def currentUser = User.findById(springSecurityService.principal.id)
             println("current user:"+currentUser)
             println("=========="+role.substring(6,9))
+            redirect(controller:'Member', action:'dashboard', params: [id: springSecurityService.principal.id])
 
-            if(role.substring(6,9).equalsIgnoreCase("stu")) {
-                redirect(controller:'Member', action:'dashboard')
-            }else if(role.substring(6,9).equalsIgnoreCase("adm")){
-                println "here = here"
-                redirect(controller:'Member', action:'adminDashboard')
-            }else if(role.substring(6,9).equalsIgnoreCase("fac")){
-                redirect(controller:'Member', action:'facultyDashboard')
-            }else if(role.substring(6,9).equalsIgnoreCase("lib")){
-                redirect(controller:'Member', action:'librarianDashboard')
-            }
         }
         else {
             redirect(controller:'login', action:'index')
         }
 
     }
-    @Secured("ROLE_STUDENT")
+
+    @Secured("permitAll")
     def dashboard() {
         render (view: "dashboard")
     }
 
-    @Secured("ROLE_ADMIN")
-    def adminDashboard(){
-        println("here")
-        render(view: "dashboard")
-    }
-
-    @Secured("ROLE_FACULTY")
-    def facultyDashboard(){
-        render(view: "dashboard")
-    }
-
-    @Secured("ROLE_LIBRARIAN")
-    def librarianDashboard(){
-        render(view: "dashboard")
-    }
 
     @Secured("permitAll")
     def history(){
